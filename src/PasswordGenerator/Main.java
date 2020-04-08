f82d7a6be787591b95bf3cc9d47d3ba56f86e942
alcupalcu <alec.chocholski@gmail.com>
2020-04-08 23:08:11 +0200

    Added first two methods for password processing

--- src/PasswordGenerator/Main.java
+++ src/PasswordGenerator/Main.java
@@ -1,9 +1,24 @@
 package PasswordGenerator;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        String text = null;
 
+        System.out.println("Provide the text and the generator will produce a password from it:");
+
+        try {
+            text = input.nextLine();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        PasswordGenerator passwordGenerator = new PasswordGenerator(text);
+        String password = passwordGenerator.getPassword();
+        System.out.println(password);
     }
 }
--- src/PasswordGenerator/PasswordGenerator.java
+++ src/PasswordGenerator/PasswordGenerator.java
@@ -1,9 +1,49 @@
 package PasswordGenerator;
 
-public class PasswordGenerator {
-    private String text;
+import java.util.ArrayList;
+import java.util.List;
 
-    protected PasswordGenerator(String text) {
-        this.text = text;
+class PasswordGenerator {
+    private ArrayList<String> password;
+
+    PasswordGenerator(String text) throws NullPointerException {
+        if (text != null) {
+            this.password = new ArrayList<>(List.of(text.split(" ")));
+            deletePunctuationMarks();
+            upperCaseFirstLetters();
+        }
+        else {
+            throw new NullPointerException();
+        }
+    }
+
+    private void deletePunctuationMarks() {
+        ArrayList<String> textProcessed = new ArrayList<>(this.password);
+        for (String word : this.password) {
+            if (word.matches("[.!?\\-]")) {
+                textProcessed.remove(word);
+            }
+        }
+        this.password = textProcessed;
+    }
+
+    private void upperCaseFirstLetters() {
+        ArrayList<String> textProcessed = new ArrayList<>();
+        for (String word : this.password) {
+            word = word.toUpperCase();
+            char firstLetter = word.charAt(0);
+            word = String.valueOf(firstLetter);
+            textProcessed.add(word);
+        }
+        this.password = textProcessed;
+    }
+
+    public String getPassword() {
+        StringBuffer sb = new StringBuffer();
+        for (String word : this.password) {
+            sb.append(word);
+        }
+        String returnPassword = new String(sb);
+        return returnPassword;
     }
 }
